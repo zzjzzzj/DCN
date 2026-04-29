2d53508be58b2cf97a750593b033bb10ee6e4e0f
zzj <2659650539@qq.com>
2026-04-29 17:27:05 +0800

    Remove datasets/fashion_mnist_master from repo and update .gitignore

--- datasets/fashion_mnist_master/test.py
+++
@@ -1,6 +0,0 @@
-from utils import  mnist_reader
-X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
-X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
-
-print(X_train.shape)
-print(y_train.shape)
